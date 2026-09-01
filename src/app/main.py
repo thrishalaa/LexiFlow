b9cdeb0da5d1831ec13195baebc8d17db3b7740f
Thrishala Challamalla <chthrishala@gmail.com>
2026-09-01 12:48:29 +0530

    intial commit

---
+++ src/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from app.api.health import router as health_router
+
+
+app = FastAPI(
+    title="LexiFlow API",
+    version="0.1.0",
+)
+
+
+app.include_router(health_router)
